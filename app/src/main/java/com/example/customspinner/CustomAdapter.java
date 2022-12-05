942dddac0d08b60a55b97d339c0a2161655ca601
NisimKrief <nk0787@bs.amalnet.k12.il>
2022-12-05 21:27:17 +0200

    Finished

--- app/src/main/java/com/example/customspinner/CustomAdapter.java
+++ app/src/main/java/com/example/customspinner/CustomAdapter.java
@@ -1,4 +1,6 @@
+package com.example.customspinner;
 
+import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -6,17 +8,24 @@ import android.widget.BaseAdapter;
 import android.widget.ImageView;
 import android.widget.TextView;
 
-import com.example.customspinner.R;
-
-
 public class CustomAdapter extends BaseAdapter {
     String countryList[];
+    String CountryCapital[];
+    Context context;
     int symbols[];
     LayoutInflater inflter;
 
+    public CustomAdapter(Context applicationContext,String[] countryList, int[] symbols, String[] CountryCapital) {
+        this.context = applicationContext;
+        this.countryList = countryList;
+        this.symbols = symbols;
+        this.CountryCapital = CountryCapital;
+        inflter = (LayoutInflater.from(applicationContext));
+    }
+
     @Override
     public int getCount() {
-        return 0;
+        return countryList.length;
     }
 
     @Override
@@ -33,8 +42,10 @@ public class CustomAdapter extends BaseAdapter {
     public View getView(int i, View view, ViewGroup viewGroup) {
         view = inflter.inflate(R.layout.custom_lv_layout, null);
         TextView country = (TextView) view.findViewById(R.id.tV);
+        TextView capital = (TextView) view.findViewById(R.id.tV2);
         ImageView symbol = (ImageView) view.findViewById(R.id.iV);
         country.setText(countryList[i]);
+        capital.setText(CountryCapital[i]);
         symbol.setImageResource(symbols[i]);
         return view;
     }
--- app/src/main/java/com/example/customspinner/MainActivity.java
+++ app/src/main/java/com/example/customspinner/MainActivity.java
@@ -3,12 +3,46 @@ package com.example.customspinner;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 
-public class MainActivity extends AppCompatActivity {
+import android.widget.Spinner;
+import android.widget.TextView;
 
+public class MainActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
+
+    String[] Medinot = {"Choose","Israel", "USA", "Saudi Arabia", "Japan", "UK", "Tunis", "Sweden", "Russia"};
+    String[] Capital = {"A Country", "Jerusalem", "Washington", "Riyadh", "Tokyo", "London", "Tunis", "Stockholm", "Moscow"};
+    String[] Population = {"", "9.364M", "331.9M", "35.4M", "125.7M", "67.33M", "11.94M", "10.42M", "143.4M"};
+    int[] Tmonot = {R.drawable.white, R.drawable.israel, R.drawable.usa, R.drawable.saudi, R.drawable.japan, R.drawable.unitedkingdom, R.drawable.tunis, R.drawable.sweden, R.drawable.russia};
+
+    Spinner spin;
+    TextView tV3;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        spin = (Spinner) findViewById(R.id.spin);
+        tV3 = (TextView) findViewById(R.id.tV3);
+        CustomAdapter adp = new CustomAdapter(getApplicationContext(), Medinot, Tmonot, Capital);
+        spin.setOnItemSelectedListener(this);
+        spin.setAdapter(adp);
+
+
     }
+
+
+    @Override
+    public void onItemSelected(AdapterView<?> adapterView, View view, int pos, long rowid) {
+        if(pos >= 1) {
+            tV3.setText("Country: " + Medinot[pos] + "\nCapital: " + Capital[pos] + "\nPopulation:  " + Population[pos]);
+        }
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> adapterView) {
+
+    }
+
+
 }
